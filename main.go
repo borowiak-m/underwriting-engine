f1bfd493a5e66556a66ef29a6dde9014859c0241
Mariusz Borowiak <142698842+borowiak-m@users.noreply.github.com>
2024-04-29 22:50:51 +0100

    chi test route

--- main.go
+++ main.go
@@ -1,5 +1,18 @@
 package main
 
-func main() {
+import (
+	"net/http"
 
+	"github.com/go-chi/chi/v5"
+)
+
+func main() {
+	router := chi.NewMux()
+	router.Get("/test", HandleTest)
+
+	http.ListenAndServe(":3000", router)
+}
+
+func HandleTest(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("test wroute orking"))
 }
