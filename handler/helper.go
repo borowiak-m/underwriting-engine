c2012c4567df7740a2e1a45c352b74d6c9486850
Mariusz Borowiak <142698842+borowiak-m@users.noreply.github.com>
2024-05-07 17:50:55 +0100

    api error struct

--- handler/helper.go
+++ handler/helper.go
@@ -6,11 +6,36 @@ import (
 	"net/http"
 )
 
+type ApiError struct {
+	StatusCode int    `json:"statusCode"`
+	Message    string `json:"message"`
+}
+
+func (e *ApiError) Error() string {
+	return e.Message
+}
+
+func NewApiError(statusCode int, err error) *ApiError {
+	return &ApiError{
+		StatusCode: statusCode,
+		Message:    err.Error(),
+	}
+}
+
 type APIfunc func(w http.ResponseWriter, r *http.Request) error
 
 func Make(h APIfunc) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		if err := h(w, r); err != nil {
+			if apiErr, ok := err.(*ApiError); ok {
+				writeJSON(w, apiErr.StatusCode, apiErr)
+			} else {
+				errResp := map[string]any{
+					"statusCode": http.StatusInternalServerError,
+					"message":    "internal server error",
+				}
+				writeJSON(w, http.StatusInternalServerError, errResp)
+			}
 			slog.Error("HTTP API error", "err", err.Error(), "path", r.URL.Path)
 		}
 	}
