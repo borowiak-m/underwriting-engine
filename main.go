33c94e1d526d93ce861c39de9b7e14d23609135a
borowiak-m <neutron.meme@proton.me>
2024-05-14 00:14:56 +0100

    postgres db

--- main.go
+++ main.go
@@ -1,6 +1,9 @@
 package main
 
 import (
+	"database/sql"
+	"fmt"
+	"log"
 	"log/slog"
 	"net/http"
 	"os"
@@ -8,12 +11,39 @@ import (
 	"github.com/borowiak-m/underwriting-engine/handler"
 	"github.com/go-chi/chi/v5"
 	"github.com/joho/godotenv"
+	"github.com/uptrace/bun"
+	"github.com/uptrace/bun/dialect/pgdialect"
+	"github.com/uptrace/bun/driver/pgdriver"
 )
+
+func getDB() *bun.DB {
+	var (
+		host     = os.Getenv("DB_HOST")
+		user     = os.Getenv("DB_USER")
+		password = os.Getenv("DB_PASSWORD")
+		name     = os.Getenv("DB_NAME")
+	)
+
+	dsn := fmt.Sprintf("postgres://%s:%s@%s/%s?sslmode=disable",
+		user,
+		password,
+		host,
+		name)
+
+	sqldb := sql.OpenDB(pgdriver.NewConnector(pgdriver.WithDSN(dsn)))
+	return bun.NewDB(sqldb, pgdialect.New())
+}
 
 func main() {
 	// config
 	if err := godotenv.Load(); err != nil {
 		panic("Error loading .env file")
+	}
+
+	// db
+	db := getDB()
+	if err := db.Ping(); err != nil {
+		log.Fatal(err)
 	}
 
 	// routes
